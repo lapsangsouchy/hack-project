de34e7346be6752f376cb5592cfb9a9d2aff228d
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2024-08-06 11:20:27 -0400

    Update

--- init-firebase.js
+++ init-firebase.js
@@ -1,12 +1,4 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: 'AIzaSyCYoLwtvHIqBvNJt8F2nEumHUxznVnDO50',
-  authDomain: 'hack-in-38f8c.firebaseapp.com',
-  databaseURL: 'https://hack-in-38f8c-default-rtdb.firebaseio.com',
-  projectId: 'hack-in-38f8c',
-  storageBucket: 'hack-in-38f8c.appspot.com',
-  messagingSenderId: '311356419377',
-  appId: '1:311356419377:web:fb0afd2941e2c148c65410',
-};
+var firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
